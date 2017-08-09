de535954349e23f0c0617783f51fe9f806dc4f4e
StanislavEsin <376825@gmail.com>
2017-08-10 01:15:12 +0600

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/*
+ *Class Calculate решение задачи части 001 урок1.
+ *@author Stanislav
+ *@since 10.08.2017
+*/
 public class Calculate {
+	/*
+	*Конструктор, вывод строки в консоль.
+	*@param arg - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Stanislav Esin (mailto:376825@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
