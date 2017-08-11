ce67ff83c447b73db90b2fbc5c2751594ae4b5c5
StanislavEsin <376825@gmail.com>
2017-08-11 21:07:56 +0600

    task 188

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,53 @@
+package ru.job4j.condition;
+
+/**
+ * Point.
+ *
+ * @author Stanislav (376825@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Point {
+    /**
+     */
+    private int x;
+    /**
+     */
+    private int y;
+
+    /**
+     * Point.
+     * @param x - int
+     * @param y - int
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * getX.
+     * @return int
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * getY.
+     * @return int
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * is.
+     * @param a - int
+     * @param b - int
+     * @return boolean
+     */
+    public boolean is(int a, int b) {
+        return (a * this.getX() + b) <= (this.getX() + this.getY()) ? true : false;
+    }
+}
