6eb4ff51afee8fd748f14bddeca71acfabdc7a5e
StanislavEsin <376825@gmail.com>
2017-08-11 20:41:50 +0600

    task 189

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.max;
+
+/**
+ * Max.
+ *
+ * @author Stanislav (376825@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * max.
+     * @param first - double
+     * @param second - double
+     * @return int
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Stanislav (376825@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MaxTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
