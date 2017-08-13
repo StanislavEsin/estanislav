3d28408265a7ffe7f2172b48582da56becbdb163
StanislavEsin <376825@gmail.com>
2017-08-13 14:26:53 +0600

    task 13559 - refactor

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -19,11 +19,7 @@ public class Board {
 
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <= width; j++) {
-                if (i % 2 == 0) {
-                    result.append(j % 2 == 0 ? "x" : " ");
-                } else {
-                    result.append(j % 2 == 0 ? " " : "x");
-                }
+                result.append((i + j) % 2 == 0 ? "x" : " ");
             }
             result.append("\r\n");
         }
