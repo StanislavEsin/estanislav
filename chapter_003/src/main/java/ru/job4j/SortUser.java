d9ec8a4e71b15f48fa1fdddd27568948407e6b2a
StanislavEsin <376825@gmail.com>
2017-09-02 14:09:20 +0600

    task 10036

--- chapter_003/src/main/java/ru/job4j/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -19,5 +20,31 @@ public class SortUser {
      */
     public Set<User> sort(List<User> list) {
         return new TreeSet<User>(list);
+    }
+
+    /**
+     * sort.
+     * @param list - List<User>
+     * @return return - List<User>
+     */
+    public List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, (user, t1) ->
+                Integer.compare(user.getName().length(), t1.getName().length()));
+
+        return list;
+    }
+
+    /**
+     * sort.
+     * @param list - List<User>
+     * @return return - List<User>
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, (user, t1) -> {
+            int compareName = user.getName().compareTo(t1.getName());
+            return compareName != 0 ? compareName : Integer.compare(user.getAge(), t1.getAge());
+        });
+
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/SortUserTest.java
@@ -36,4 +36,46 @@ public class SortUserTest {
 
         assertThat(result, is(expected));
     }
+
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenListUsersThenListUsersSortedByLengthOfName() {
+        List<User> listUsers = new ArrayList<>();
+        listUsers.add(new User(1, "user123", "city1", 21));
+        listUsers.add(new User(1, "user1", "city1", 15));
+        listUsers.add(new User(1, "user12", "city1", 34));
+
+        List<User> result = new SortUser().sortNameLength(listUsers);
+
+        List<User> expected = new ArrayList<>();
+        expected.add(new User(1, "user1", "city1", 15));
+        expected.add(new User(1, "user12", "city1", 34));
+        expected.add(new User(1, "user123", "city1", 21));
+
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenListUsersThenListUsersSortedByTwoFieldsNameAndAge() {
+        List<User> listUsers = new ArrayList<>();
+        listUsers.add(new User(1, "Сергей", "city1", 25));
+        listUsers.add(new User(1, "Иван", "city1", 30));
+        listUsers.add(new User(1, "Сергей", "city1", 20));
+        listUsers.add(new User(1, "Иван", "city1", 25));
+
+        List<User> result = new SortUser().sortByAllFields(listUsers);
+
+        List<User> expected = new ArrayList<>();
+        expected.add(new User(1, "Иван", "city1", 25));
+        expected.add(new User(1, "Иван", "city1", 30));
+        expected.add(new User(1, "Сергей", "city1", 20));
+        expected.add(new User(1, "Сергей", "city1", 25));
+
+        assertThat(result, is(expected));
+    }
 }
