36d22384c41fd1b600725997293a820d7126847f
StanislavEsin <376825@gmail.com>
2017-09-16 15:29:09 +0600

    task 159

---
+++ chapter_004/src/main/java/ru/job4j/list/MyLinkedList.java
@@ -0,0 +1,144 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * MyLinkedList.
+ * @param <E>
+ *
+ * @author Stanislav (376825@mail.ru)
+ * @since 15.09.2017
+ */
+public class MyLinkedList<E> implements Iterable<E> {
+    /**
+     */
+    private int size = 0;
+    /**
+     */
+    private MyLinkedList.Node<E> first;
+    /**
+     */
+    private MyLinkedList.Node<E> last;
+
+    /**
+     * add.
+     * @param item - E
+     * @return return - boolean
+     */
+    public boolean add(E item) {
+        MyLinkedList.Node last = this.last;
+        MyLinkedList.Node newNode = new MyLinkedList.Node(last, item, null);
+        this.last = newNode;
+        if (last == null) {
+            this.first = newNode;
+        } else {
+            last.next = newNode;
+        }
+
+        ++this.size;
+        return true;
+    }
+
+    /**
+     * get.
+     * @param index - int
+     * @return return - E
+     */
+    public E get(int index) {
+        if (!(index >= 0 && index < this.size)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
+        }
+
+        return this.node(index).item;
+    }
+
+    /**
+     * node - определяем с какой стороны выгоднее итерироваться.
+     * @param index - int
+     * @return return - MyLinkedList.Node<E>
+     */
+    MyLinkedList.Node<E> node(int index) {
+        MyLinkedList.Node var2;
+        int var3;
+        if (index < this.size >> 1) {
+            var2 = this.first;
+
+            for (var3 = 0; var3 < index; ++var3) {
+                var2 = var2.next;
+            }
+
+            return var2;
+        } else {
+            var2 = this.last;
+
+            for (var3 = this.size - 1; var3 > index; --var3) {
+                var2 = var2.prev;
+            }
+
+            return var2;
+        }
+    }
+
+    /**
+     * Node.
+     * @param <E>
+     */
+    private static class Node<E> {
+        /**
+         */
+        private E item;
+        /**
+         */
+        private MyLinkedList.Node<E> next;
+        /**
+         */
+        private MyLinkedList.Node<E> prev;
+
+        /**
+         * Constructor.
+         * @param prev - MyLinkedList.Node<E>
+         * @param item - E
+         * @param next - MyLinkedList.Node<E>
+         */
+        private Node(MyLinkedList.Node<E> prev, E item, MyLinkedList.Node<E> next) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyLinkedList.Itr();
+    }
+
+    /**
+     * Itr.
+     */
+    private class Itr implements Iterator<E> {
+        /**
+         */
+        private MyLinkedList.Node cursor;
+        /**
+         */
+        private int nextIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return this.nextIndex < MyLinkedList.this.size;
+        }
+
+        @Override
+        public E next() {
+            if (this.nextIndex >= MyLinkedList.this.size) {
+                throw new NoSuchElementException();
+            }
+
+            this.cursor = this.cursor == null ? MyLinkedList.this.first : this.cursor.next;
+            ++nextIndex;
+
+            return (E) cursor.item;
+        }
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/list/MyLinkedListTest.java
@@ -0,0 +1,86 @@
+package ru.job4j.list;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Test.
+ *
+ * @author Stanislav (376825@mail.ru)
+ * @since 16.09.2017
+ */
+public class MyLinkedListTest {
+    /**
+     */
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddValueThenShouldReturnTheSameValue() {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add("test1");
+        myLinkedList.add("test2");
+
+        String result = myLinkedList.get(1);
+
+        assertThat(result, is("test2"));
+    }
+
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenIteratorGetNextCallThenShouldPointerForward() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(2);
+        myLinkedList.add(5);
+        myLinkedList.add(8);
+
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        iterator.next();
+        iterator.next();
+        int result = iterator.next();
+
+        assertThat(result, is(8));
+    }
+
+    /**
+     * Test add.
+     * @throws IndexOutOfBoundsException - .
+     */
+    @Test
+    public void whenGetIndexOutOfBoundsThenIndexOutOfBoundsException() {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add("test1");
+
+        thrown.expect(IndexOutOfBoundsException.class);
+
+        myLinkedList.get(1);
+    }
+
+    /**
+     * Test add.
+     * @throws NoSuchElementException - .
+     */
+    @Test
+    public void whenGetNextCallThenArrayIndexOutOfBoundsException() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(2);
+
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        iterator.next();
+
+        thrown.expect(NoSuchElementException.class);
+        iterator.next();
+    }
+}
