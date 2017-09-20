63f4c408ff8aa7b7fb0d5c446b15e4d66b1052a8
StanislavEsin <376825@gmail.com>
2017-09-20 13:19:11 +0600

    task 1005

---
+++ chapter_004/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Test.
+ *
+ * @author Stanislav (376825@mail.ru)
+ * @since 20.09.2017
+ */
+public class UserTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddTwoUsersDoNotOverrideEqualsHashCode() {
+        User userA = new User("test", 1, Calendar.getInstance());
+        User userB = new User("test", 1, Calendar.getInstance());
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(userA, new Object());
+        map.put(userB, new Object());
+
+        System.out.println(map);
+    }
+}
